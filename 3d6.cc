4b7b78fa28c625eb1d81c54a21898913ce750bdd
Damien Sullivan <phoenix@mindstalk.net>
2016-12-27 14:46:43 -0500

    trying valarray

--- 3d6.cc
+++ 3d6.cc
@@ -1,5 +1,6 @@
 #include "s.h"
 #include <random>
+#include <valarray>
 
 int main() {
     auto eng = default_random_engine{ static_cast<unsigned>(time(nullptr))};
@@ -7,11 +8,15 @@ int main() {
             
     //vector<int> hist{20}; doesn't work... oh, setting to the single
     //element 20
-    vector<int> hist(20);
+    valarray<int> hist(20);
     for (int i=0; i<100000; i++) {
         auto sum = die()+die()+die();
         hist[sum]++;
     }
+    for (int i=2; i<=19; i++) {
+        cout << i << ":" << hist[i] << "\n";
+    }
+    hist *= 2;
     for (int i=2; i<=19; i++) {
         cout << i << ":" << hist[i] << "\n";
     }
